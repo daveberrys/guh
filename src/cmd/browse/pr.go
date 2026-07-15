e2cc0e10068e6d55253bb7fada7dccd91dc0d00c
daveberrys <84567826+daveberrys@users.noreply.github.com>
2026-07-15 20:19:14 +0800

    implement pr and issues browse

--- src/cmd/browse/pr.go
+++ src/cmd/browse/pr.go
@@ -3,25 +3,18 @@ package browse
 import (
 	"strings"
 
-	"github.com/daveberrys/guh/src/cmd"
 	"github.com/daveberrys/guh/src/utils"
 	"github.com/spf13/cobra"
 	"github.com/pkg/browser"
 )
 
-func init() {
-	cmd.RootCmd.AddCommand(createCmd)
-	createCmd.AddCommand(prCmd)
-	createCmd.AddCommand(issuesCmd)
-}
-
-var createCmd = &cobra.Command{
-	Use:   "browse",
-	Short: "Opens a browser to the repository.",
+var prCmd = &cobra.Command{
+	Use:   "pr",
+	Short: "Opens a browser to the PRs page.",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		url, err := utils.RunGit(true, "remote", "get-url", "origin")
 		if err != nil { return err }
 		url = strings.TrimSuffix(url, ".git")
-		return browser.OpenURL(url)
+		return browser.OpenURL(url + "/pulls")
 	},
 }
