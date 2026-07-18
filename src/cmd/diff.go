49d1efe4c98c4f4e3a731ed926e2a66dc5d89fec
daveberrys <84567826+daveberrys@users.noreply.github.com>
2026-07-19 03:06:58 +0800

    diff specific file

--- src/cmd/diff.go
+++ src/cmd/diff.go
@@ -8,10 +8,11 @@ import (
 func init() { rootCmd.AddCommand(diffCmd) }
 
 var diffCmd = &cobra.Command{
-	Use:   "diff",
+	Use:   "diff [file:optional]",
 	Short: "Show working tree changes",
-	Args:  cobra.NoArgs,
+	Args:  cobra.MaximumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) == 1 { return utils.RunGitSequence(false, []string{"diff", args[0]}) }
 		return utils.RunGitSequence(false, []string{"diff"})
 	},
 }
